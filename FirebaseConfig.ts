af0fead2a2d5777721e5577e4626cd19bb932a34
Rômulo Vargas <romulorvargas@gmail.com>
2026-05-20 21:41:22 -0300

    Remove sensitive Firebase configuration
    Removed Firebase configuration details for security reasons.

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAlBgc0exquJBcRDz3BOT1L3m5cnUv0xBY",
-    authDomain: "projeto-progdispmoveis.firebaseapp.com",
-    projectId: "projeto-progdispmoveis",
-    storageBucket: "projeto-progdispmoveis.firebasestorage.app",
-    messagingSenderId: "814425908289",
-    appId: "1:814425908289:web:70ffb78389aa18697492c0",
-    measurementId: "G-F64W78NWRY"
-  };
+  //Adicionar aqui as infos do projeto do Firebase
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
